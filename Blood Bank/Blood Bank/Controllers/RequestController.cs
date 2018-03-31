dea05ba5a97bc032ebd3fe12ef493b9504578475
7Mahfuz <thehry01@gmail.com>
2018-03-31 11:24:51 +0600

    Request Crud operation Added

--- Blood Bank/Blood Bank/Controllers/RequestController.cs
+++ Blood Bank/Blood Bank/Controllers/RequestController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Blood_Bank.Logics;
+using DataLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,67 @@ namespace Blood_Bank.Controllers
 {
     public class RequestController : Controller
     {
+        private UnitOfWork uow = null;
+        public RequestController()
+        {
+            uow = new UnitOfWork();
+        }
+        public RequestController(UnitOfWork _uow)
+        {
+            this.uow = _uow;
+        }
         // GET: Request
-        public ActionResult Index()
+        public ActionResult AllRequests(int id)
+        {
+            //string UserName = Session["UserName"].ToString();
+            //User user = uow.Repository<User>().GetModel(x => x.UserName == UserName);
+            var model = uow.Repository<Request>().GetList(x=>x.UserId==id);
+            return View(model);
+        }
+
+        public ActionResult Add()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Request model)
+        {
+            string UserName = Session["UserName"].ToString();
+            User user = uow.Repository<User>().GetModel(x => x.UserName == UserName);
+
+            model.UserId = user.Id;
+            uow.Repository<Request>().InsertModel(model);
+            uow.Save();
+
+            return RedirectToAction("AllRequests", new { model.id });
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            var model = uow.Repository<Request>().GetModelById(id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(Request model)
+        {
+            uow.Repository<Request>().UpdateModel(model);
+            uow.Save();
+            return RedirectToAction("AllRequests", new { model.id });
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            var model = uow.Repository<Request>().GetModelById(id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Delete(Request model)
+        {
+            uow.Repository<Request>().DeleteModel(model);
+            uow.Save();
+            return RedirectToAction("AllRequests", new { model.id });
         }
     }
 }
